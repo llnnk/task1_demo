1ca68a258843a808c996ac62d2c271e9e885bd3c
llnnk <oleg.lysenko.06@gmail.com>
2025-07-12 14:59:05 +0300

    Update task3.sql

--- task3.sql
+++ task3.sql
@@ -1,5 +1,5 @@
-SELECT departure_airport, arrival_airport, AVG (actual_departure - scheduled_departure ) AS average_delay
+SELECT departure_airport, arrival_airport, AVG (actual_departure - scheduled_departure ) AS average_delay -- Выбираем столбцы 
 FROM flights
-where flights.status in ('Departed', 'Arrived') and actual_departure -  scheduled_departure IS NOT NULL
+where flights.status in ('Departed', 'Arrived') and actual_departure IS NOT null -- Выбираем только те рейсы, которые вылетили и прибыли и исключаем записи у которых нет даты вылета
 GROUP BY departure_airport, arrival_airport
-ORDER BY average_delay DESC
+ORDER BY average_delay desc -- сортируем по убыванию 
